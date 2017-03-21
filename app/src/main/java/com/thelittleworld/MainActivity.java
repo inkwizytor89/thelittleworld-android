8230cebb9031d78cc07890e5dc15f79ab76630b4
Kamil Kostyszyn <inkwizytor89@gmail.com>
2017-03-21 17:42:47 +0100

    add ItemsActivity

--- app/src/main/java/com/thelittleworld/MainActivity.java
+++ app/src/main/java/com/thelittleworld/MainActivity.java
@@ -1,13 +1,41 @@
 package com.thelittleworld;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    public List<String> list = new ArrayList<String>();
+    public ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        arrayAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, list);
+        ListView actionsListView = (ListView) findViewById(R.id.actions_listView);
+        actionsListView.setAdapter(arrayAdapter);
+
+
+        onItemsButtonClick();
+    }
+
+    private void onItemsButtonClick() {
+        final Button button = (Button) findViewById(R.id.items_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                ListView actionsListView = (ListView) findViewById(R.id.actions_listView);
+                list.add("Kocham Sylwie !!");
+                arrayAdapter.notifyDataSetChanged();
+            }
+        });
     }
 }
