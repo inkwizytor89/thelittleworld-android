a2467a2c14480289270b732a6bbbc5cef4542a40
Kamil Kostyszyn <inkwizytor89@gmail.com>
2017-04-01 21:52:59 +0200

    add examply request to google with failed button "button" (on debug error, on run is ok)

--- app/src/main/java/com/thelittleworld/MainActivity.java
+++ app/src/main/java/com/thelittleworld/MainActivity.java
@@ -3,10 +3,18 @@ package com.thelittleworld;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +36,39 @@ public class MainActivity extends AppCompatActivity {
 
         onItemsButtonClick();
         onAddEventButtonClick();
+
+
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Log.e("example", "Elo benc");
+
+                // Instantiate the RequestQueue.
+                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                String url = "http://www.google.com";
+
+                // Request a string response from the provided URL.
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // Display the first 500 characters of the response string.
+                                Log.e("example", "Response is: " + response.substring(0, 500));
+                                klik();
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("example", error.getMessage());
+                    }
+                });
+// Add the request to the RequestQueue.
+                queue.add(stringRequest);
+            }
+        });
+
+
     }
 
     private void onItemsButtonClick() {
@@ -44,9 +85,13 @@ public class MainActivity extends AppCompatActivity {
         final Button button = (Button) findViewById(R.id.addEvent_button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                list.add("Kocham Sylwie !!");
-                arrayAdapter.notifyDataSetChanged();
+                klik();
             }
         });
+    }
+
+    private void klik() {
+        list.add("Kocham Sylwie !!");
+        arrayAdapter.notifyDataSetChanged();
     }
 }
